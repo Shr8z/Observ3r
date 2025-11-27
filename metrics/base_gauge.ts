b556569e76e57b096ff31c0aa9cf1a3154e5620c
Shr8z <shr8z@outlook.com>
2025-11-28 00:08:16 +0100

    feat: remove unused Gauge metrics from base_gauge and moonwell_gauge files

--- metrics/base_gauge.ts
+++
@@ -1,7 +0,0 @@
-import { Gauge } from 'prom-client';
-
-export const token_balance = new Gauge({
-  name: 'base_token_balance',
-  help: 'Base token balance for each token',
-  labelNames: ['address', 'token']
-});
